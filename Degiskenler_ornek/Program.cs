a88b125da269d7226f6e38704b8127fc6b64fc31
denizdogruel00 <denizyazilm@gmail.com>
2026-02-25 16:22:15 +0300

    Add project files.

---
+++ Degiskenler_ornek/Program.cs
@@ -0,0 +1,8 @@
+﻿string stringveriable = "ahmet";
+int sayi = 25;
+double ondaliklisayi = 3.14;
+Console.WriteLine("adiniz: "+ stringveriable);
+Console.WriteLine("tamsayi: "+sayi);
+Console.WriteLine("ondalikli sayi: " + ondaliklisayi);
+Console.ReadKey();
+
