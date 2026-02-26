5d66d8be177b18e854021239a361d3fe32317da4
denizdogruel00 <denizyazilm@gmail.com>
2026-02-26 13:46:45 +0300

    yas hesabı eklendi ve proje bitti

--- Aritmetik_operator_ornek/Program.cs
+++ Aritmetik_operator_ornek/Program.cs
@@ -8,14 +8,14 @@
             //int sinav2 = 60;
             //int ort = (sinav1 + sinav2) / 2;
             //Console.WriteLine($"Ortalamaniz: {ort}");
-            int faturatutari,komisyon;
-            double kdvoran, odenecektutar, toplamtutar;
-            faturatutari = 500;
-            komisyon =25;
-            kdvoran = 0.15;
-            odenecektutar = (faturatutari * (1+kdvoran));
-            toplamtutar = odenecektutar + komisyon;
-            Console.WriteLine($"Ödenecek Tutar:{toplamtutar}");
+            //int faturatutari,komisyon;
+            //double kdvoran, odenecektutar, toplamtutar;
+            //faturatutari = 500;
+            //komisyon =25;
+            //kdvoran = 0.15;
+            //odenecektutar = (faturatutari * (1+kdvoran));
+            //toplamtutar = odenecektutar + komisyon;
+            //Console.WriteLine($"Ödenecek Tutar:{toplamtutar}");
             Console.ReadKey();
 
 
---
+++ yashesabi/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace yashesabi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dyili = 2001;
+            int simdikiyil = 2026;
+            int yas = simdikiyil - dyili;
+            Console.WriteLine($"Yaş: {yas}");
+            Console.ReadKey();
+        }
+    }
+}
