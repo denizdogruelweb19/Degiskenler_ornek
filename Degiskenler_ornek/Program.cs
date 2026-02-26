f874f87919e57bef8d92397f88233d2a66238cc5
denizdogruel00 <denizyazilm@gmail.com>
2026-02-26 11:25:15 +0300

    değişkenler bitti

--- Degiskenler_ornek/Program.cs
+++ Degiskenler_ornek/Program.cs
@@ -1,7 +1,7 @@
-﻿string stringveriable = "ahmet";
+﻿string stringdeğişken = "ahmet";
 int sayi = 25;
 double ondaliklisayi = 3.14;
-Console.WriteLine("adiniz: "+ stringveriable);
+Console.WriteLine("adiniz: "+ stringdeğişken);
 Console.WriteLine("tamsayi: "+sayi);
 Console.WriteLine("ondalikli sayi: " + ondaliklisayi);
 Console.ReadKey();
