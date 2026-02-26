cdc015e11d09120143a78e0599ac23cc69f60e16
denizdogruel00 <denizyazilm@gmail.com>
2026-02-26 12:52:39 +0300

    kdvli fatura hesabı uygulaması yapıldı

---
+++ Aritmetik_Operatorler/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Aritmetik_Operatorler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 3 + 5;
+            Console.WriteLine($"Toplam: {sum}");
+            int a = 30;
+            int b = 10;
+            //fark
+            int c = a - b;
+            Console.WriteLine($"{a} - {b}={c}");
+            //çarpım
+            int d = a * b;
+            Console.WriteLine($"{a} * {b}={d}");
+            //bölüm
+            int e = a / b;
+            Console.WriteLine($"{a} / {b}={e}");
+            //mod
+            int f = a % b;
+            Console.WriteLine($"{a} % {b}={f}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Aritmetik_operator_ornek/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Aritmetik_operator_ornek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int sinav1 = 50;
+            //int sinav2 = 60;
+            //int ort = (sinav1 + sinav2) / 2;
+            //Console.WriteLine($"Ortalamaniz: {ort}");
+            int faturatutari,komisyon;
+            double kdvoran, odenecektutar, toplamtutar;
+            faturatutari = 500;
+            komisyon =25;
+            kdvoran = 0.15;
+            odenecektutar = (faturatutari * (1+kdvoran));
+            toplamtutar = odenecektutar + komisyon;
+            Console.WriteLine($"Ödenecek Tutar:{toplamtutar}");
+            Console.ReadKey();
+
+
+            Console.ReadKey();
+        }
+    }
+}
